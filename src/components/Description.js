596929bc86102e42f1a30a3f214b96f490d656c2
opsaran <prakashom.iitkgp@gmail.com>
2022-02-19 13:10:31 +0530

    style: fixed some issues

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,7 @@ import About from "./about.js";
 import Skillset from "./skillset.js";
 import Projects from "./projects.js";
 import Contact from "./contact.js";
-
+import myself from "../images/myselfnew.png";
 const Description = () => {
   return (
     <>
@@ -11,6 +11,23 @@ const Description = () => {
         <div className="intro">
           <p className="italicise">Enter the world of</p>
           <h1>Om Prakash Saran</h1>
+          <div className="typing-text-effect">
+            <h2>I'm a</h2>
+            <ul className="changing-text">
+              <li>
+                <span>Web Developer</span>
+              </li>
+              <li>
+                <span>Freelancer</span>
+              </li>
+              <li>
+                <span>Problem Solver</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src={myself} alt="" />
         </div>
       </section>
 
